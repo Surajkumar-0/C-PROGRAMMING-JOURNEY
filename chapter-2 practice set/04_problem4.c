0b696685a5f75bc010e5e0b2f9596b6dfd3c2b63
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-19 16:23:31 +0530

    Solved Chapter-2 Practice Set Problems in C.

---
+++ chapter-2 practice set/04_problem4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int x=2,y=3,z=3,k=1;
+    printf("%d\n", 3*x/y - z + k); // prints 0
+    return 0;
+}
---
+++ chapter-2 practice set/05_problem5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    float b = 3.0;
+    float result = b + a;   // int a ko float me convert kar diya jayega
+
+    printf("Result of 3.0 + 1 = %f\n", result);
+    return 0;
+}
