906cec068ccb3cb5a7aab4f95b837def00b80e51
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-21 23:17:46 +0530

    Solved Chapter-3 Practice Set Problems in C.

---
+++ chapter-3 practice set/06_problem6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, greatest;
+
+    // Input four numbers
+    printf("Enter four numbers: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    // Assume a is greatest initially
+    greatest = a;
+
+    if (b > greatest)
+        greatest = b;
+
+    if (c > greatest)
+        greatest = c;
+
+    if (d > greatest)
+        greatest = d;
+
+    printf("The greatest number is: %d\n", greatest);
+
+    return 0;
+}
