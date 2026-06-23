ecd113b85ca59ad5ccce0ab1f6cd5971e20a1494
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-23 11:29:39 +0530

    Completed Chapter : 4 - Loops in C.

---
+++ chapter 4/01_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // print Happy Birthday 1 lakh times
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+    printf("Happy Birthday !\n");
+
+    return 0;
+}
---
+++ chapter 4/06_increment_decrement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+    i = i + 5;
+    printf("The value of i is %d\n", i);
+    printf("The value of i is %d\n", i++);
+
+    i += 2;
+    printf("The value of i is %d\n", i);
+
+    return 0;
+}
